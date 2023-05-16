4142bd3402db834db04895c5e80bbe8f6c655f67
Minseo Kim <kimminseo@gimminseoui-MacBookPro.local>
2023-05-16 16:41:45 +0900

    second commit

--- main.cpp
+++ main.cpp
@@ -2,14 +2,34 @@
 
 using namespace std;
 
-int main(){
+//drawing a shape
+// int main(){
 
-    cout << "    /|" << endl;
-    cout << "   / |" << endl;
-    cout << "  /  |" << endl;
-    cout << " /   |" << endl;
-    cout << "/____|" << endl;
+//     cout << "    /|" << endl;
+//     cout << "   / |" << endl;
+//     cout << "  /  |" << endl;
+//     cout << " /   |" << endl;
+//     cout << "/____|" << endl;
 
-    return 0;
+//     return 0;
 
-}
+// }
+
+
+
+//variable
+// int main(){
+
+//     string characterName = "Peter";
+//     int characterAge;
+//     characterAge = 25;
+
+//     cout << "There once was a man named " << characterName << endl;
+//     cout << "He was " << characterAge << " years old" << endl;
+
+//     characterName = "Matthew";
+//     cout << "He liked the name " << characterName << endl;
+//     cout << "But he did not like being " << characterAge << endl;
+
+//     return 0;
+// }
