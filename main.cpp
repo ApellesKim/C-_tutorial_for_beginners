bb9453bb5ff65466eed6ad6ffc01dc4046205658
Minseo Kim <kimminseo@gimminseoui-MacBookPro.local>
2023-05-17 10:34:28 +0900

    third commit

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 //drawing a shape
-// int main(){
+// int main()
+//{
 
 //     cout << "    /|" << endl;
 //     cout << "   / |" << endl;
@@ -18,7 +19,8 @@ using namespace std;
 
 
 //variable
-// int main(){
+// int main()
+//{
 
 //     string characterName = "Peter";
 //     int characterAge;
@@ -31,5 +33,41 @@ using namespace std;
 //     cout << "He liked the name " << characterName << endl;
 //     cout << "But he did not like being " << characterAge << endl;
 
+//     return 0;
+// }
+
+
+
+// Data types
+// int main ()
+// {
+//     char grade = 'A';
+//     string phrase = "Giraffe Academy";
+//     int age = 25;
+//     double gpa = 4.58;
+//     bool isMale = true;
+
+//     cout << grade << endl;
+
+//     return 0;
+// }
+
+
+
+// Working with strings
+// int main()
+// {
+    
+//     string phrase = "Giraffe Academy";
+//     phrase [1] = 'y';
+//     string phrasesubstr;
+//     phrasesubstr = phrase.substr (3,4);
+
+//     cout << phrase << endl;
+//     cout << phrase.length() << endl;
+//     cout << phrase[1] << endl;
+//     cout << phrase.find("Academy", 0) << endl;
+//     cout << phrase.substr(3, 4) << endl;
+//     cout << phrasesubstr << endl;
 //     return 0;
 // }
