83faa3c4bc968882a0e5f208dd816215bb19e47b
Minseo Kim <kimminseo@gimminseoui-MacBookPro.local>
2023-05-18 10:33:28 +0900

    fourth commit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -70,4 +71,41 @@ using namespace std;
 //     cout << phrase.substr(3, 4) << endl;
 //     cout << phrasesubstr << endl;
 //     return 0;
+// }
+
+
+
+//Working with numbers
+// int main ()
+// {
+    // int a = 3;
+    // int b = 9;
+    // int wnum = 5;
+    // double dnum = 5.5;
+
+    // wnum ++; // add 1 to wnum
+    // cout << wnum << endl;
+    // wnum --; // minus 1 from wnum
+    // cout << wnum << endl;
+    // wnum += 90; //add 90 to wnum
+    // cout << wnum << endl;
+
+
+    // cout << a + b << endl;
+    // cout << a - b << endl;
+    // cout << a / b << endl;
+    // cout << a * b << endl;
+    // cout << a % b << endl; // Find a remainder
+
+
+    // cout << pow (2,5) << endl;
+    // cout << sqrt (26) << endl;
+    // cout << round (2.64444233311) << endl;
+    // cout << ceil (2.1) << endl; // round up
+    // cout << floor (2.8) << endl; // round down
+    // cout << fmax (2.6478, 6/3.5) << endl; // find the maximum number
+    // cout << fmin (2.6478, 6/3.5) << endl; // find the minimum number
+
+
+    // return 0;
 // }
