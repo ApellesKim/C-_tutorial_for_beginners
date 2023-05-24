d52f9ad089dff4565f49713b85870fa0a409743b
ApellesKim <minseo.apelles.kim@gmail.com>
2023-05-24 15:08:02 +0900

    twelfth commit

--- main.cpp
+++ main.cpp
@@ -175,13 +175,17 @@ using namespace std;
 
 
 // Arrays
-int main ()
-{
-    int luckyNums[] = {3, 5, 14, 35, 46, 57, 66, 923};
+// int main ()
+// {
+//     int luckyNums[] = {3, 5, 14, 35, 46, 57, 66, 923};
 
-    luckyNums[0] = 59;
+//     luckyNums[0] = 59;
 
-    cout << luckyNums[0];
+//     cout << luckyNums[0];
 
-    return 0;
-}
+//     return 0;
+// }
+
+
+
+
